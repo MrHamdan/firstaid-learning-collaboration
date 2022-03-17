e1b78323ff2f15fb0a4a8c8db7132d26b35fb20a
MrHamdan <mr.hamdanahmed@gmail.com>
2022-03-17 13:58:58 +0600

    fixing design

--- src/components/CourseOverview/CourseCurriculam.js
+++ src/components/CourseOverview/CourseCurriculam.js
@@ -26,14 +26,14 @@ const CourseCurriculam = () => {
                 <Container maxWidth='xl'>
                     <Box sx={{ marginTop: '73px' }}>
                         <Tagline>Course Overview</Tagline>
-                        <Typography sx={{ fontSize: '18px', fontWeight: '500', fontStyle: 'normal', lineHeight: '32px' }}>By the end of the course, learners will be able to: <br />
+                        <Typography sx={{ fontSize: '18px', fontWeight: '500', fontStyle: 'normal', lineHeight: '32px', marginTop: '45px' }}>By the end of the course, learners will be able to: <br />
                             This infection control training course is designed to provide those who work in a healthcare or social care setting with the skills and knowledge to stop of the spread of bacteria and risk of infection in the workplace. <br />
                             Treating a casualty with First Aid is a hard but rewarding experience. Being able to help a child in distress or pain is an especially admirable talent and one that all parents or child-minders should ideally know. By the end of the course, you’ll know how to interact with a young casualty, how to keep them calm, and when to call an emergency. It should prove to hold invaluable information for keeping children and babies safe from illness, and potentially life-threatening conditions.<br />
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. At elit, gravida vitae ac elementum tellus. Justo quam vestibulum aenean pellentesque posuere eu. Euismod mattis mi dictum id. Pellentesque arcu sagittis, id et vitae convallis. Vitae quam et, urna quis nunc, enim leo sit phasellus. Lectus non id non pharetra, sed aliquet bibendum eleifend sagittis. Ac et volutpat facilisis scelerisque purus etiam interdum. Est amet, iaculis sem in mauris netus rutrum. Morbi vitae quis quis eu ut vel at sodales. Eros, purus velit ut vel vitae sit. At interdum sagittis, viverra vitae amet, justo arcu dolor, tortor.</Typography>
                     </Box>
                     <Box sx={{ marginTop: '99px' }}>
                         <Tagline>Course Curriculam</Tagline>
-                        <Grid container spacing={2}>
+                        <Grid container spacing={2} sx={{ marginTop: '53px' }}>
                             <Grid item xs={12} xl={8}>
                                 <Item sx={{ backgroundColor: 'transparent', textAlign: 'left', boxShadow: '0' }}>
                                     <Accordion>
