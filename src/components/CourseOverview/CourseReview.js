4be3721e39610c6dc2668479b7468f1fd2b2e39e
MrHamdan <you@example.com>
2022-03-18 00:36:39 +0600

    detail page review section design finished

--- src/components/CourseOverview/CourseReview.js
+++ src/components/CourseOverview/CourseReview.js
@@ -21,7 +21,7 @@ const Item = styled(Paper)(({ theme }) => ({
 const CourseReview = () => {
     return (
         <>
-            <Box sx={{ height: '800px' }}>
+            <Box sx={{ height: { xl: '800px', xs: '1200px' } }}>
                 <Container maxWidth='xl'>
                     <Box sx={{ display: 'flex', position: 'relative', alignItems: 'center', marginTop: '40px' }} >
                         <Tagline>Reviews of This Course</Tagline> <Box sx={{ marginLeft: '30px' }}><Image src={reviewDesign} />
@@ -29,7 +29,82 @@ const CourseReview = () => {
                                 color: '#FFB300'
                             }} /> 4.8 User Rating</Typography></Box>
                     </Box>
-
+                    <Box sx={{ flexGrow: 1, paddingTop: '100px' }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} xl={6} md={12}>
+                                <Item>
+                                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                            <Box>
+                                                <Image src={conorOne} />
+                                            </Box>
+                                            <Box sx={{ marginTop: '-80px' }}>
+                                                <Typography>Connor Morgan</Typography>
+                                            </Box>
+                                        </Box>
+                                        <Box sx={{ marginTop: '-80px', marginRight: '30px' }}>
+                                            <Typography>3 days Ago</Typography>
+                                        </Box>
+                                    </Box>
+                                    <Box><Typography sx={{ textAlign: 'left', marginLeft: '40px', marginTop: '-50px' }}>This course has some very interesting tips that one can apply when communicating with your customers. Some of these tips are extremely valuable and useful! This is by far one of the best.</Typography></Box>
+                                </Item>
+                            </Grid>
+                            <Grid item xs={12} xl={6} md={12}>
+                                <Item>
+                                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                            <Box>
+                                                <Image src={conorOne} />
+                                            </Box>
+                                            <Box sx={{ marginTop: '-80px' }}>
+                                                <Typography>Connor Morgan</Typography>
+                                            </Box>
+                                        </Box>
+                                        <Box sx={{ marginTop: '-80px', marginRight: '30px' }}>
+                                            <Typography>3 days Ago</Typography>
+                                        </Box>
+                                    </Box>
+                                    <Box><Typography sx={{ textAlign: 'left', marginLeft: '40px', marginTop: '-50px' }}>This course has some very interesting tips that one can apply when communicating with your customers. Some of these tips are extremely valuable and useful! This is by far one of the best.</Typography></Box>
+                                </Item>
+                            </Grid>
+                            <Grid item xs={12} xl={6} md={12}>
+                                <Item>
+                                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                            <Box>
+                                                <Image src={conorOne} />
+                                            </Box>
+                                            <Box sx={{ marginTop: '-80px' }}>
+                                                <Typography>Connor Morgan</Typography>
+                                            </Box>
+                                        </Box>
+                                        <Box sx={{ marginTop: '-80px', marginRight: '30px' }}>
+                                            <Typography>3 days Ago</Typography>
+                                        </Box>
+                                    </Box>
+                                    <Box><Typography sx={{ textAlign: 'left', marginLeft: '40px', marginTop: '-50px' }}>This course has some very interesting tips that one can apply when communicating with your customers. Some of these tips are extremely valuable and useful! This is by far one of the best.</Typography></Box>
+                                </Item>
+                            </Grid>
+                            <Grid item xs={12} xl={6} md={12}>
+                                <Item>
+                                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                            <Box>
+                                                <Image src={conorOne} />
+                                            </Box>
+                                            <Box sx={{ marginTop: '-80px' }}>
+                                                <Typography>Connor Morgan</Typography>
+                                            </Box>
+                                        </Box>
+                                        <Box sx={{ marginTop: '-80px', marginRight: '30px' }}>
+                                            <Typography>3 days Ago</Typography>
+                                        </Box>
+                                    </Box>
+                                    <Box><Typography sx={{ textAlign: 'left', marginLeft: '40px', marginTop: '-50px' }}>This course has some very interesting tips that one can apply when communicating with your customers. Some of these tips are extremely valuable and useful! This is by far one of the best.</Typography></Box>
+                                </Item>
+                            </Grid>
+                        </Grid>
+                    </Box>
                 </Container>
             </Box>
         </>
