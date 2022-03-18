4a362429439161b09eeeb473715c9990594cd68f
Hamim <rahmanhamiminfo@gmail.com>
2022-03-18 09:44:59 +0600

    footer

--- src/Redux/reducers/quizReducer.js
+++ src/Redux/reducers/quizReducer.js
@@ -1,28 +1,23 @@
-<<<<<<< HEAD
 const initialState = {
-    quiz: [],
-=======
-const initialState={
-    quiz:[],
-    finalAnswers: [],
->>>>>>> 3fc2e37e6ac4b286d4812bf56f2e6e4554eba063
-    loading: true,
-}
+ quiz: [],
+ finalAnswers: [],
+ loading: true,
+};
 
 export const quizReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "FETCH_QUIZ":
-            return {
-                ...state,
-                quiz: action.payload,
-                loading: false
-            }
-        case "SUBMIT_QUIZ":
-            return {
-                ...state,
-                finalAnswers: action.payload,
-            }   
-        default:
-            return state
-    }
-}
+ switch (action.type) {
+  case "FETCH_QUIZ":
+   return {
+    ...state,
+    quiz: action.payload,
+    loading: false,
+   };
+  case "SUBMIT_QUIZ":
+   return {
+    ...state,
+    finalAnswers: action.payload,
+   };
+  default:
+   return state;
+ }
+};
