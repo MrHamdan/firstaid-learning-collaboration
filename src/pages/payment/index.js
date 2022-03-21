9636f7b0fc60ac89a276162d966e8718838ee265
MrHamdan <mr.hamdanahmed@gmail.com>
2022-03-21 10:32:54 +0600

    adding title to the payment page

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -1,8 +1,13 @@
 import Payment from "components/Payment/Payment";
+import Head from "next/head";
 
 const index = () => {
     return (
         <>
+            <Head>
+                <title>Payment</title>
+                <link rel="shortcut icon" href="favicon2.ico"></link>
+            </Head>
             <Payment />
         </>
     );
