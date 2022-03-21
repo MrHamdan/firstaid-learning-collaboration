2a8ca58b6ed654440ca97f1e04b91e52d7982b96
Ashfakul <ashfakulkarim2@gmail.com>
2022-03-21 10:52:40 +0600

    Quiz level Finding

--- src/components/Quiz/ResultsHome.js
+++ src/components/Quiz/ResultsHome.js
@@ -1,4 +1,5 @@
 import { Box, Container } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux"
 import Answer from "./Answer";
 import QuizResultHeader from "./QuizResultHeader";
@@ -7,19 +8,48 @@ import QuizResultHeader from "./QuizResultHeader";
 const ResultsHome = () => {
 
     const answer = useSelector(state => state.allQuiz.finalAnswers)
-     console.log(answer);
+    const [level,setLevel]=useState();
+    let beginner = 0;
+    let intermediate = 0;
+    let advanced = 0;
+
+  
      let correctAnswerArray =[]
+    //  let quizAnswerlevel =[]
         answer.forEach((element) => {
            element.options.forEach((option) => {
                 if(option.isCorrect && element.selectedAnswer.isCorrect){
                     correctAnswerArray.push(option.option)
+                    // quizAnswerlevel.push(element.level)
                 }
            })
-        })
-    
+           if (element.selectedAnswer.isCorrect && element.level === 'beginner') {
+            beginner++;
+          }
+          else if (element.selectedAnswer.isCorrect && element.level === 'intermediate') {
+             intermediate++;
+        }
+        else if (element.selectedAnswer.isCorrect && element.level === 'advanced') {
+            advanced++;
+        }
 
-    console.log(correctAnswerArray);
-  
+        })
+        useEffect(() => {
+          if (beginner > intermediate || beginner > advanced) {
+            setLevel('beginner')
+          }
+         else if (intermediate > beginner || intermediate > advanced) {
+            setLevel('intermediate')
+         }
+          else if (advanced > beginner || advanced > intermediate) {
+            setLevel('advanced');
+          }
+         else {
+            setLevel('beginner');
+           }
+        },[beginner,advanced,intermediate])
+
+    console.log(level);
   return (
       
     <Container sx={{my:1}}>
