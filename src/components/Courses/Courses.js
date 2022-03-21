cd23dc1c205d48e255ffbc6c4ddc7a4dd47b37e1
Ahmed Labeeb <labeeb.edu123@gmail.com>
2022-03-21 11:51:53 +0600

    without carousel

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -8,16 +8,14 @@ import Content from '../shared/Content';
 import Tagline from '../shared/Tagline'
 import { useDispatch, useSelector } from 'react-redux';
 
-// import { addToCart } from '../../Redux/actions/cartAction';
-
 
 
 
 const Courses = ({ courses }) => {
-
     const dispatch = useDispatch();
     const sliderRef = useRef(null);
- 
+
+
     return (
         <div>
             <Container sx={{
@@ -27,21 +25,26 @@ const Courses = ({ courses }) => {
                     xl: 'xl'
                 }
             }}>
-            <Tagline text={<div>Interacting First Aid Courses</div>} />
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: { xs: 'column', md: 'row' } }}>
-                <Content text={<div>We are a company who is best known for offering awarding body accredited courses for anyone who wishes to <br /> take their professional life to the next level.</div>} />
-                <Box >
-                    <CarouselButton sliderRef={sliderRef} />
+                <Tagline>Interacting First Aid Courses</Tagline>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: { xs: 'column', md: 'row' } }}>
+                    <Content>
+                        <Typography component='span'>
+                            We are a company who is best known for offering awarding body accredited courses for anyone who wishes to <br /> take their professional life to the next level.
+                        </Typography>
+                    </Content>
+                    <Box >
+                        <CarouselButton sliderRef={sliderRef} />
+                    </Box>
                 </Box>
-            </Box>
 
-            <Box sx={{ backgroundColor: '#FEF9F7', mt: '30px', padding: '10px' }}>
-                <Carousel sliderRef={sliderRef}
-                    content={courses?.map(course => (
-                        <CourseCard key={course.id} course={course} />
-                    ))}
-                />
-            </Box>
+                <Box sx={{ backgroundColor: '#FEF9F7', mt: '30px', padding: '10px' }}>
+                    {/*<Carousel sliderRef={sliderRef}
+                        content={courses?.map(course => (
+                            <CourseCard key={course.id} course={course} />
+                        ))}
+                        />*/}
+                </Box>
+
 
             </Container>
         </div>
@@ -49,3 +52,12 @@ const Courses = ({ courses }) => {
 };
 
 export default Courses;
+
+
+// <Box sx={{ backgroundColor: '#FEF9F7', mt: '30px', padding: '10px' }}>
+//                     <Carousel sliderRef={sliderRef}
+//                         content={courses?.map(course => (
+//                             <CourseCard key={course.id} course={course} />
+//                         ))}
+//                     />
+//                 </Box>
