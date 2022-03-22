a53edb38c98351d743adb02e70febb4165230041
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-03-22 15:43:36 +0600

    conflict in carousel fixed

--- src/components/Shared/Carousel.js
+++ src/components/Shared/Carousel.js
@@ -3,57 +3,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const Carousel = ({
- content,
- sliderRef,
- slidesToShow,
- slidesToScroll,
- arrows,
- dots,
- center,
-}) => {
- const settings = {
-  arrows: arrows || false,
-  dots: dots || false,
-  infinite: false,
-  speed: 100,
-  centerMode: false,
-  slidesToShow: slidesToShow || 4,
-  slidesToScroll: slidesToScroll || 4,
-  initialSlide: 0,
-  responsive: [
-   {
-    breakpoint: 1024,
-    settings: {
-     slidesToShow: slidesToShow || 2,
-     slidesToScroll: slidesToScroll || 1,
-     infinite: false,
-     dots: dots || false,
-    },
-   },
-   {
-    breakpoint: 600,
-    settings: {
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     initialSlide: 0,
-     infinite: false,
-     dots: dots || false,
-    },
-   },
-   {
-    breakpoint: 480,
-    settings: {
-     slidesToShow: 1,
-     slidesToScroll: 1,
-     infinite: false,
-     dots: dots || false,
-    },
-   },
-  ],
- };
 
-<<<<<<< HEAD
 const Carousel = ({ content, sliderRef, slidesToShow, slidesToScroll, arrows, dots, center }) => {
 
     const settings = {
@@ -104,15 +54,7 @@ const Carousel = ({ content, sliderRef, slidesToShow, slidesToScroll, arrows, do
         </Slider>
 
     );
-=======
- return (
-  <div>
-   <Slider ref={sliderRef} {...settings}>
-    {content}
-   </Slider>
-  </div>
- );
->>>>>>> hamim
 };
+
 
 export default Carousel;
