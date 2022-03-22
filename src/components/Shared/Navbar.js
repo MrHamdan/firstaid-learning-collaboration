78bba3310cc21baa2169db824e9315bed048d31d
Hamim <rahmanhamiminfo@gmail.com>
2022-03-22 13:02:48 +0600

    nav

--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -1,14 +1,14 @@
 import {
-    AppBar,
-    Badge,
-    Button,
-    Container,
-    IconButton,
-    InputAdornment,
-    Menu,
-    TextField,
-    Toolbar,
-    Typography,
+ AppBar,
+ Badge,
+ Button,
+ Container,
+ IconButton,
+ InputAdornment,
+ Menu,
+ TextField,
+ Toolbar,
+ Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import Link from "next/link";
@@ -24,201 +24,205 @@ import logo from "../../images/logo.png";
 import useAuth from "hooks/useAuth";
 import { useSelector } from "react-redux";
 const Styles = {
-    navlink: {
-        color: "#4A5074",
-        fontWeight: "bold",
-        fontSize: {
-            lg: "20.175px",
-        },
-        textTransform: "capitalize",
-    },
-    loginButton: {
-        fontWeight: "bold",
-        borderRadius: "11.21px",
-        width: {
-            xs: "100px",
-            md: "152.43px",
-        },
-        height: "67.25px",
-    },
+ navlink: {
+  color: "#4A5074",
+  fontWeight: "bold",
+  fontSize: {
+   lg: "20.175px",
+  },
+  textTransform: "capitalize",
+ },
+ loginButton: {
+  fontWeight: "bold",
+  borderRadius: "11.21px",
+  width: {
+   xs: "100px",
+   md: "152.43px",
+  },
+  height: "67.25px",
+ },
 };
 const Navbar = () => {
-    const state = useSelector(state => state.cart);
-    const { cart } = state;
-    const router = useRouter();
-    const { user, logOut, open, setOpen } = useAuth();
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+ const state = useSelector((state) => state.cart);
+ const { cart } = state;
+ const router = useRouter();
+ const { user, logOut, open, setOpen } = useAuth();
+ const handleOpen = () => setOpen(true);
+ const handleClose = () => setOpen(false);
 
-    const [anchorElNav, setAnchorElNav] = useState(null);
+ const [anchorElNav, setAnchorElNav] = useState(null);
 
-    const handleOpenNavMenu = (event) => {
-        setAnchorElNav(event.currentTarget);
-    };
+ const handleOpenNavMenu = (event) => {
+  setAnchorElNav(event.currentTarget);
+ };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+ const handleCloseNavMenu = () => {
+  setAnchorElNav(null);
+ };
 
-    const gotoCart = () => {
-        router.push("/cart");
-    };
-    return (
-        <AppBar
-            position="static"
-            sx={{
-                backgroundColor: "white",
-                boxShadow: "none",
-                padding: "40px 0px",
-            }}
-        >
-            <Container
-                sx={{
-                    maxWidth: {
-                        lg: "lg",
-                        xl: "xl",
-                    },
-                }}
-            >
-                <Toolbar disableGutters>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
-                    >
-                        <Image src={logo} alt="" />
-                    </Typography>
+ const gotoCart = () => {
+  router.push("/cart");
+ };
+ return (
+  <AppBar
+   position="static"
+   sx={{
+    backgroundColor: "white",
+    boxShadow: "none",
+    padding: "40px 0px",
+   }}
+  >
+   <Container
+    sx={{
+     maxWidth: {
+      lg: "lg",
+      xl: "xl",
+     },
+    }}
+   >
+    <Toolbar disableGutters>
+     <Typography
+      variant="h6"
+      noWrap
+      component="div"
+      sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+     >
+      <Image src={logo} alt="" />
+     </Typography>
 
-                    {user.email && (
-                        <Typography sx={{ color: "black" }}> Signed in: {user?.email}</Typography>
-                    )}
+     {user.email && (
+      <Typography sx={{ color: "black" }}> Signed in: {user?.email}</Typography>
+     )}
 
-                    <Box>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
-                        >
-                            <Image src={logo} alt="" />
-                        </Typography>
-                        <TextField
-                            id="outlined-basic"
-                            variant="outlined"
-                            placeholder="Search for courses"
-                            sx={{
-                                mt: 2,
-                                display: {
-                                    md: "none",
-                                },
-                            }}
-                        />
-                    </Box>
+     <Box>
+      <Typography
+       variant="h6"
+       noWrap
+       component="div"
+       sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+      >
+       <Image src={logo} alt="" />
+      </Typography>
+      <TextField
+       id="outlined-basic"
+       variant="outlined"
+       placeholder="Search for courses"
+       sx={{
+        mt: 2,
+        display: {
+         md: "none",
+        },
+       }}
+      />
+     </Box>
 
-                    <Box
-                        sx={{
-                            flexGrow: 1,
-                            justifyContent: "space-evenly",
-                            alignItems: "center",
-                            display: {
-                                xs: "none",
-                                md: "flex",
-                            },
-                        }}
-                    >
-                        <TextField id="outlined-basic" placeholder="Search for courses" variant="outlined" />
+     <Box
+      sx={{
+       flexGrow: 1,
+       justifyContent: "space-evenly",
+       alignItems: "center",
+       display: {
+        xs: "none",
+        md: "flex",
+       },
+      }}
+     >
+      <TextField
+       id="outlined-basic"
+       placeholder="Search for courses"
+       variant="outlined"
+      />
 
-                        <Box
-                            sx={{
-                                display: "flex",
-                                justifyContent: "space-between",
-                                flexBasis: "500px",
-                                alignItems: "center",
-                            }}
-                        >
-                            <Link href="/" style={{ textDecoration: "none" }} passHref>
-                                <Button sx={Styles.navlink}>Home</Button>
-                            </Link>
-                            <Link href="/#courses" passHref>
-                                <Button sx={Styles.navlink}>All Courses</Button>
-                            </Link>
-                            <Link href="/blogs" style={{ textDecoration: "none" }} passHref>
-                                <Button sx={Styles.navlink}>Blogs</Button>
-                            </Link>
-                            <Button sx={Styles.navlink}>Contact</Button>
+      <Box
+       sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        flexBasis: "500px",
+        alignItems: "center",
+       }}
+      >
+       <Link href="/" style={{ textDecoration: "none" }} passHref>
+        <Button sx={Styles.navlink}>Home</Button>
+       </Link>
+       <Link href="/#courses" passHref>
+        <Button sx={Styles.navlink}>All Courses</Button>
+       </Link>
+       <Link href="/blogs" style={{ textDecoration: "none" }} passHref>
+        <Button sx={Styles.navlink}>Blogs</Button>
+       </Link>
+       <Button sx={Styles.navlink}>Contact</Button>
 
-                            <Button>
-                                <Badge badgeContent={cart.length} color="error">
-                                    <FaShoppingCart
-                                        onClick={gotoCart}
-                                        style={{ ...Styles.navlink, fontSize: "25px" }}
-                                    />
-                                </Badge>
-                            </Button>
-                        </Box>
-                    </Box>
-                    <Box sx={{ flexGrow: 0 }}>
-                        <PrimaryButton onClick={!user.email ? handleOpen : logOut}>
-                            {!user.email ? "Login" : "Logout"}
-                        </PrimaryButton>
+       <Button>
+        <Badge badgeContent={cart.length} color="error">
+         <FaShoppingCart
+          onClick={gotoCart}
+          style={{ ...Styles.navlink, fontSize: "25px" }}
+         />
+        </Badge>
+       </Button>
+      </Box>
+     </Box>
+     <Box sx={{ flexGrow: 0 }}>
+      <PrimaryButton onClick={!user.email ? handleOpen : logOut}>
+       {!user.email ? "Login" : "Logout"}
+      </PrimaryButton>
 
-                        <LoginModal handleClose={handleClose} open={open} />
-                    </Box>
+      <LoginModal handleClose={handleClose} open={open} />
+     </Box>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                            sx={{ color: "black" }}
-                        >
-                            <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: "bottom",
-                                horizontal: "left",
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: "top",
-                                horizontal: "left",
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: "block", md: "none" },
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                }}
-                            >
-                                <Link href="/" style={{ textDecoration: "none" }} passHref>
-                                    <Button sx={Styles.navlink}>Home</Button>
-                                </Link>
-                                <Link href="/#courses" passHref>
-                                    <Button sx={Styles.navlink}>All Courses</Button>
-                                </Link>
-                                <Button sx={Styles.navlink}>Blogs</Button>
-                                <Button sx={Styles.navlink}>Contact</Button>
+     <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+      <IconButton
+       size="large"
+       aria-label="account of current user"
+       aria-controls="menu-appbar"
+       aria-haspopup="true"
+       onClick={handleOpenNavMenu}
+       color="inherit"
+       sx={{ color: "black" }}
+      >
+       <MenuIcon />
+      </IconButton>
+      <Menu
+       id="menu-appbar"
+       anchorEl={anchorElNav}
+       anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+       }}
+       keepMounted
+       transformOrigin={{
+        vertical: "top",
+        horizontal: "left",
+       }}
+       open={Boolean(anchorElNav)}
+       onClose={handleCloseNavMenu}
+       sx={{
+        display: { xs: "block", md: "none" },
+       }}
+      >
+       <Box
+        sx={{
+         display: "flex",
+         flexDirection: "column",
+        }}
+       >
+        <Link href="/" style={{ textDecoration: "none" }} passHref>
+         <Button sx={Styles.navlink}>Home</Button>
+        </Link>
+        <Link href="/#courses" passHref>
+         <Button sx={Styles.navlink}>All Courses</Button>
+        </Link>
+        <Button sx={Styles.navlink}>Blogs</Button>
+        <Button sx={Styles.navlink}>Contact</Button>
 
-                                <FaShoppingCart onClick={gotoCart} style={Styles.navlink} />
-                            </Box>
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+        <FaShoppingCart onClick={gotoCart} style={Styles.navlink} />
+       </Box>
+      </Menu>
+     </Box>
+    </Toolbar>
+   </Container>
+  </AppBar>
+ );
 };
 
 export default Navbar;
