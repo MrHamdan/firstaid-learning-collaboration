7820526e9c7e6f4695e62484e40b1bc0bc4d023d
Hamim <rahmanhamiminfo@gmail.com>
2022-03-22 15:26:22 +0600

    nav scroll

--- src/components/Shared/Carousel.js
+++ src/components/Shared/Carousel.js
@@ -1,60 +1,65 @@
-import React from 'react';
-import Slider from 'react-slick';
+import React from "react";
+import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+const Carousel = ({
+ content,
+ sliderRef,
+ slidesToShow,
+ slidesToScroll,
+ arrows,
+ dots,
+ center,
+}) => {
+ const settings = {
+  arrows: arrows || false,
+  dots: dots || false,
+  infinite: false,
+  speed: 100,
+  centerMode: false,
+  slidesToShow: slidesToShow || 4,
+  slidesToScroll: slidesToScroll || 4,
+  initialSlide: 0,
+  responsive: [
+   {
+    breakpoint: 1024,
+    settings: {
+     slidesToShow: slidesToShow || 2,
+     slidesToScroll: slidesToScroll || 1,
+     infinite: false,
+     dots: dots || false,
+    },
+   },
+   {
+    breakpoint: 600,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     initialSlide: 0,
+     infinite: false,
+     dots: dots || false,
+    },
+   },
+   {
+    breakpoint: 480,
+    settings: {
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     infinite: false,
+     dots: dots || false,
+    },
+   },
+  ],
+ };
 
-const Carousel = ({ content, sliderRef, slidesToShow, slidesToScroll, arrows, dots, center }) => {
-
-    const settings = {
-        arrows: arrows || false,
-        dots: dots || false,
-        infinite: false,
-        speed: 100,
-        centerMode: false,
-        slidesToShow: slidesToShow || 4,
-        slidesToScroll: slidesToScroll || 4,
-        initialSlide: 0,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: slidesToShow || 2,
-                    slidesToScroll: slidesToScroll || 1,
-                    infinite: false,
-                    dots: dots || false
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    initialSlide: 0,
-                    infinite: false,
-                    dots: dots || false
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: false,
-                    dots: dots || false
-                }
-            }
-        ]
-    };
-
-
-    return (
-        <div>
-            <Slider ref={sliderRef} {...settings}>
-                {content}
-            </Slider>
-        </div>
-    );
+ return (
+  <div>
+   <Slider ref={sliderRef} {...settings}>
+    {content}
+   </Slider>
+  </div>
+ );
 };
 
 export default Carousel;
